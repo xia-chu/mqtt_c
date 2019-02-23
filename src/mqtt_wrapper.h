519708ea3aeb7235536b7e9fd35d5d2a1eea871b
xiongziliang <771730766@qq.com>
2019-02-23 17:02:14 +0800

    添加注释

--- src/mqtt_wrapper.h
+++ src/mqtt_wrapper.h
@@ -37,15 +37,37 @@ do{\
  */
 typedef struct {
     /**
-     * 对象输出协议数据
+     * 对象输出协议数据，请调用send发送给服务器
      * @param arg 用户数据指针
-     * @param iov
-     * @param iovcnt
+     * @param iov 数据块
+     * @param iovcnt 数据块个数
      */
     int (*mqtt_data_output)(void *arg, const struct iovec *iov, int iovcnt);
+
+    /**
+     * 登录服务器结果回调
+     * @param arg 用户数据指针
+     * @param flags @see MqttConnectFlag
+     * @param ret_code 0代表成功，@see MqttRetCode
+     */
     void (*mqtt_handle_conn_ack)(void *arg, char flags, char ret_code);
+
+    /**
+     * 心跳包回复
+     * @param arg 用户数据指针
+     */
     void (*mqtt_handle_ping_resp)(void *arg);
 
+    /**
+     * 收到服务器主题发布的消息
+     * @param arg 用户数据指针
+     * @param pkt_id 数据包号
+     * @param topic 主题
+     * @param payload 消息负载
+     * @param payloadsize 消息长度
+     * @param dup 是否为重复包
+     * @param qos qos等级，如果是'只发送一次'类型，那么还将触发mqtt_handle_publish_rel回调
+     */
     void (*mqtt_handle_publish)(void *arg,
                                 uint16_t pkt_id,
                                 const char *topic,
@@ -54,9 +76,17 @@ typedef struct {
                                 int dup,
                                 enum MqttQosLevel qos);
 
+    /**
+     * 服务器释放消息，qos类型为'只发送一次'
+     * @param arg 用户数据指针
+     * @param pkt_id pkt_id 数据包号
+     */
     void (*mqtt_handle_publish_rel)(void *arg,
                                     uint16_t pkt_id);
 
+    /**
+     * 回调用户数据指针，本结构体回调函数第一个参数即此参数
+     */
     void *_user_data;
 } mqtt_callback;
 //////////////////////////////////////////////////////////////////////
