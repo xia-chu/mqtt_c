4e2afb14df3a4893fc68d4ffd21b86d6b41a4296
xiongziliang <771730766@qq.com>
2019-07-01 11:49:43 +0800

    可替换memory相关函数

--- src/include/jimi_memory.h
+++ src/include/jimi_memory.h
@@ -8,6 +8,19 @@
 extern "C" {
 #endif // __cplusplus
 
+///////////////////内存相关函数指针声明/////////////////////////////
+typedef void *(*malloc_ptr)(int size);
+typedef void (*free_ptr)(void *ptr);
+typedef void *(*realloc_ptr)(void *ptr,int size);
+typedef char *(*strdup_ptr)(const char *str);
+
+///////////////////替换内存相关的函数/////////////////////////////
+void set_malloc_ptr(malloc_ptr ptr);
+void set_free_ptr(free_ptr ptr);
+void set_realloc_ptr(realloc_ptr ptr);
+void set_strdup_ptr(strdup_ptr ptr);
+
+///////////////////内存相关的函数/////////////////////////////
 void *jimi_malloc(int size);
 void jimi_free(void *ptr);
 void *jimi_realloc(void *ptr,int size);
--- src/source/jimi_memory.c
+++ src/source/jimi_memory.c
@@ -7,28 +7,50 @@
 #include "jimi_memory.h"
 #include "jimi_log.h"
 
+static malloc_ptr s_malloc_ptr = NULL;
+static free_ptr s_free_ptr = NULL;
+static realloc_ptr s_realloc_ptr = NULL;
+static strdup_ptr s_strdup_ptr = NULL;
+
+
+void set_malloc_ptr(malloc_ptr ptr){
+    s_malloc_ptr = ptr;
+}
+
+void set_free_ptr(free_ptr ptr){
+    s_free_ptr = ptr;
+}
+
+void set_realloc_ptr(realloc_ptr ptr){
+    s_realloc_ptr = ptr;
+}
+
+void set_strdup_ptr(strdup_ptr ptr){
+    s_strdup_ptr = ptr;
+}
+
 void *jimi_malloc(int size){
-    void *ptr = malloc(size);
+    void *ptr = s_malloc_ptr ? s_malloc_ptr(size) : malloc(size);
     CHECK_PTR(ptr,NULL);
     return ptr;
 }
 
 void jimi_free(void *ptr){
     CHECK_PTR(ptr,);
-    free(ptr);
+    s_free_ptr ? s_free_ptr(ptr) : free(ptr);
 }
 
 void *jimi_realloc(void *ptr,int size){
     CHECK_PTR(ptr,NULL);
     CHECK_PTR(size,NULL);
-    void *ret = realloc(ptr,size);
+    void *ret = s_realloc_ptr ? s_realloc_ptr(ptr,size) : realloc(ptr,size);
     CHECK_PTR(ret,NULL);
     return ret;
 }
 
 char *jimi_strdup(const char *str){
     CHECK_PTR(str,NULL);
-    char *ret = strdup(str);
+    char *ret = s_strdup_ptr ? s_strdup_ptr(str) : strdup(str);
     CHECK_PTR(ret,NULL);
     return ret;
 }
