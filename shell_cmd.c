967ec91e94b2788b8c79a4091a1797e6d631c57d
xiongziliang <771730766@qq.com>
2019-07-12 18:07:52 +0800

    完成GPIO输出测试

--- shell_cmd.c
+++ shell_cmd.c
@@ -4,15 +4,16 @@
 #include "jimi_shell.h"
 #include "jimi_log.h"
 
-#define FAILED "Failed: "
-#define SUCCESS "OK: "
+
+#define FAILED_STR "Failed: "
+#define SUCCESS_STR "OK: "
 
 
 //////////////////////////////////////////////////////////version命令////////////////////////////////////////////////////
 
 #define VERSION "1.0.0"
 static void on_complete_version(void *user_data, printf_func func,cmd_context *cmd,opt_map all_opt) {
-    func(user_data,SUCCESS"%s\r\n",VERSION);
+    func(user_data,SUCCESS_STR"%s\r\n",VERSION);
 }
 
 //////////////////////////////////////////////////////////setmqtt命令////////////////////////////////////////////////////
@@ -23,7 +24,7 @@ static char mqtt_server[32] = {0};
 static option_value_ret on_option_setmqtt_server(void *user_data,printf_func func,cmd_context *cmd,const char *opt_long_name,const char *opt_val){
     const char *pos = strstr(opt_val,":");
     if(!pos){
-        func(user_data,FAILED"%d %s\r\n",-1, "mqtt服务器地址无效，必须包含冒号分隔地址与端口号");
+        func(user_data,FAILED_STR"%d %s\r\n",-1, "mqtt服务器地址无效，必须包含冒号分隔地址与端口号");
         return ret_interrupt;
     }
     return ret_continue;
@@ -33,34 +34,54 @@ static void on_complete_setmqtt(void *user_data, printf_func func,cmd_context *c
     strcpy(mqtt_client_id,opt_map_get_value(all_opt,"client_id"));
     strcpy(mqtt_secret,opt_map_get_value(all_opt,"secret"));
     strcpy(mqtt_server,opt_map_get_value(all_opt,"server"));
-    func(user_data,SUCCESS"%s %s %s\r\n",mqtt_client_id,mqtt_secret,mqtt_server);
+    func(user_data,SUCCESS_STR"%s %s %s\r\n",mqtt_client_id,mqtt_secret,mqtt_server);
 }
 
 //////////////////////////////////////////////////////////getmqtt命令////////////////////////////////////////////////////
 
 static void on_complete_getmqtt(void *user_data, printf_func func,cmd_context *cmd,opt_map all_opt){
-    func(user_data,SUCCESS"%s %s %s\r\n",mqtt_client_id,mqtt_secret,mqtt_server);
+    func(user_data,SUCCESS_STR"%s %s %s\r\n",mqtt_client_id,mqtt_secret,mqtt_server);
 }
 
 //////////////////////////////////////////////////////////setio命令//////////////////////////////////////////////////////
-
+#ifdef __alios__
+#include "aos/kernel.h"
+#include "aos/hal/gpio.h"
+#include "soc_init.h"
+void set_gpio(int port, int config, int type){
+    switch (type){
+        case 0:
+            hal_gpio_output_low(&brd_gpio_table[port]);
+            break;
+        case 1:
+            hal_gpio_output_high(&brd_gpio_table[port]);
+            break;
+        default:
+            hal_gpio_output_toggle(&brd_gpio_table[port]);
+            break;
+    }
+}
+#endif
 static void on_complete_setio(void *user_data, printf_func func,cmd_context *cmd,opt_map all_opt){
-    int flag = atoi(opt_map_get_value(all_opt,"flag"));
-    int point = atoi(opt_map_get_value(all_opt,"point"));
-    func(user_data,SUCCESS"已经设置管脚:%d电平为:%d\r\n",point,flag ? 1 : 0 );
+    int port = atoi(opt_map_get_value(all_opt,"port"));
+    int type = atoi(opt_map_get_value(all_opt,"type"));
+#ifdef __alios__
+    set_gpio(port,OUTPUT_PUSH_PULL,type);
+#endif
+    func(user_data,SUCCESS_STR"已经设置管脚:%d电平为:%d\r\n",port,type);
 }
 
 //////////////////////////////////////////////////////////getio命令//////////////////////////////////////////////////////
 
 static void on_complete_getio(void *user_data, printf_func func,cmd_context *cmd,opt_map all_opt){
     int point = atoi(opt_map_get_value(all_opt,"point"));
-    func(user_data,SUCCESS"%d %d\r\n",point,1);
+    func(user_data,SUCCESS_STR"%d %d\r\n",point,1);
 }
 
 //////////////////////////////////////////////////////////getadc命令//////////////////////////////////////////////////////
 
 static void on_complete_getadc(void *user_data, printf_func func,cmd_context *cmd,opt_map all_opt){
-    func(user_data,SUCCESS"%0.2f\r\n",1.28);
+    func(user_data,SUCCESS_STR"%0.2f\r\n",1.28);
 }
 
 //////////////////////////////////////////////////////////setpwm命令//////////////////////////////////////////////////////
@@ -70,19 +91,19 @@ static float ratio = 0.5f;
 static void on_complete_setpwm(void *user_data, printf_func func,cmd_context *cmd,opt_map all_opt){
     hz = atoi(opt_map_get_value(all_opt,"hz"));
     ratio = atof(opt_map_get_value(all_opt,"ratio"));
-    func(user_data,SUCCESS"%d %0.2f\r\n",hz,ratio);
+    func(user_data,SUCCESS_STR"%d %0.2f\r\n",hz,ratio);
 }
 
 //////////////////////////////////////////////////////////getpwm命令//////////////////////////////////////////////////////
 
 static void on_complete_getpwm(void *user_data, printf_func func,cmd_context *cmd,opt_map all_opt){
-    func(user_data,SUCCESS"%d %0.2f\r\n",hz,ratio);
+    func(user_data,SUCCESS_STR"%d %0.2f\r\n",hz,ratio);
 }
 
 //////////////////////////////////////////////////////////reboot命令//////////////////////////////////////////////////////
 
 static void on_complete_reboot(void *user_data, printf_func func,cmd_context *cmd,opt_map all_opt){
-    func(user_data,SUCCESS"系统将重启!\r\n");
+    func(user_data,SUCCESS_STR"系统将重启!\r\n");
 }
 
 //////////////////////////////////////////////////////////regist_cmd/////////////////////////////////////////////////////
@@ -112,8 +133,8 @@ void regist_cmd(){
     {
         //设置gpio
         cmd_context *cmd = cmd_context_alloc("setio", "设置gpio管脚电平", on_complete_setio);
-        cmd_context_add_option_must(cmd, NULL, 'p', "point", "gpio管脚号，范围 0 ~ 16(假设有16脚)");
-        cmd_context_add_option_must(cmd, NULL, 'f', "flag", "gpio口电压高低,0或1");
+        cmd_context_add_option_must(cmd, NULL, 'p', "port", "gpio管脚号，范围 0 ~ 16(假设有16脚)");
+        cmd_context_add_option_must(cmd, NULL, 't', "type", "gpio口电压高低,0或1");
         cmd_regist(cmd);
     }
 
