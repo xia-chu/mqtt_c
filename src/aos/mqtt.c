da3feaf6befa7eaef42cd6ee2b072cfc01d2ee2a
xiongziliang <771730766@qq.com>
2019-07-17 11:30:01 +0800

    优化命名

--- src/aos/mqtt.c
+++ src/aos/mqtt.c
@@ -17,12 +17,6 @@
 #include <atparser.h>
 #endif
 
-#define CLIENT_ID "IMEI17328379634"
-#define USER_NAME "JIMIMAX"
-#define SECRET "3ec79d7a4da932faf834c15b687d8caf"
-#define SERVER_IP "39.108.84.233"
-#define SERVER_PORT 1883
-
 typedef struct {
     //iot_cxt对象
     void *_ctx;
@@ -30,13 +24,16 @@ typedef struct {
     int _fd;
 } iot_user_data;
 
-//数据结构体
-static iot_user_data user_data;
-static int timer_ms = 3000;
-static aos_timer_t timer;
+static char s_user_name[] = "JIMIMAX";
+static char s_client_id[32] = "IMEI17328379634";
+static char s_secret[32] = "3ec79d7a4da932faf834c15b687d8caf";
+static char s_server_ip[16] = "39.108.84.233";
+static int  s_server_port = 1883;
+static int  s_timer_ms = 3000;
 
-extern void init_sensor();
+static void on_timer(iot_user_data *user_data);
 extern void set_gpio(int port, int config, int type);
+extern void init_sensor();
 
 /**
  * 发送数据至网络
@@ -66,16 +63,6 @@ static int send_data_to_sock(void *arg, const struct iovec *iov, int iovcnt){
 }
 
 
-//////////////////////////////////////////////////////////////////////
-/**
- * 定时器触发回调
- * @param user_data 用户数据指针
- */
-static void on_timer_tick(iot_user_data *user_data){
-    iot_timer_schedule(user_data->_ctx);
-}
-
-
 /**
  * 收到服务器下发的端点数据回调
  * @param arg 用户数据指针,即本结构体的_user_data参数
@@ -83,7 +70,7 @@ static void on_timer_tick(iot_user_data *user_data){
  * @param req_id 本次请求id
  * @param data 端点数据，只读
  */
-static void iot_on_message(void *arg,int req_flag, uint32_t req_id, iot_data *data){
+static void on_iot_message(void *arg,int req_flag, uint32_t req_id, iot_data *data){
     switch (data->_tag_id){
         case 210112:
             //led0
@@ -102,20 +89,20 @@ static void iot_on_message(void *arg,int req_flag, uint32_t req_id, iot_data *da
     }
 }
 
-static void on_sensor_event(input_event_t *event, void *priv_data) {
+static void on_event(input_event_t *event, iot_user_data *user_data) {
     switch (event->type){
         case EV_BUTTON:
             switch (event->code){
                 case CODE_BUTTON_0:{
-                    iot_send_bool_pkt(user_data._ctx,210120,event->value);
+                    iot_send_bool_pkt(user_data->_ctx,210120,event->value);
                 }
                     break;
                 case CODE_BUTTON_1:{
-                    iot_send_bool_pkt(user_data._ctx,210121,event->value);
+                    iot_send_bool_pkt(user_data->_ctx,210121,event->value);
                 }
                     break;
                 case CODE_BUTTON_2:{
-                    iot_send_bool_pkt(user_data._ctx,210122,event->value);
+                    iot_send_bool_pkt(user_data->_ctx,210122,event->value);
                 }
                     break;
                 default:
@@ -127,10 +114,12 @@ static void on_sensor_event(input_event_t *event, void *priv_data) {
     }
 }
 
-static void on_timer(void *timer, void *arg){
+static void on_timer(iot_user_data *user_data){
+    //定时器
+    iot_timer_schedule(user_data->_ctx);
     buffer buffer;
     buffer_init(&buffer);
-    iot_buffer_start(&buffer,1,iot_get_request_id(user_data._ctx));
+    iot_buffer_start(&buffer,1,iot_get_request_id(user_data->_ctx));
 
     {
         float x,y,z;
@@ -163,8 +152,10 @@ static void on_timer(void *timer, void *arg){
         }
     }
 
-    iot_send_buffer(user_data._ctx,&buffer);
+    iot_send_buffer(user_data->_ctx,&buffer);
     buffer_release(&buffer);
+    //下次执行
+    aos_post_delayed_action(s_timer_ms,on_timer,user_data);
 }
 
 /**
@@ -172,12 +163,18 @@ static void on_timer(void *timer, void *arg){
  * @param arg 用户数据指针
  * @param ret_code 错误代码，0为成功
  */
-void iot_on_connect(void *arg, char ret_code){
-    LOGD("ret_code :%d",ret_code);
+void on_iot_connect(iot_user_data *arg, char ret_code){
     if(ret_code == 0){
-        init_sensor();
-        aos_register_event_filter(EV_BUTTON, on_sensor_event, NULL);
-        aos_timer_new(&timer,on_timer,NULL,timer_ms,1);
+        LOGI("登录mqtt服务器成功");
+        static int s_flag = 0;
+        if(!s_flag){
+            s_flag = true;
+            init_sensor();
+            aos_register_event_filter(EV_BUTTON, on_event, arg);
+            on_timer(arg);
+        }
+    }else{
+        LOGW("登录mqtt服务器失败:%d",ret_code);
     }
 }
 
@@ -190,20 +187,18 @@ void run_main(){
     set_log_level(log_trace);
 
     //网络层连接服务器
-    user_data._fd = net_connet_server(SERVER_IP,SERVER_PORT,3);
+    static iot_user_data user_data;
+    user_data._fd = net_connet_server(s_server_ip,s_server_port,3);
     if(user_data._fd  == -1){
         return ;
     }
     //回调函数列表
-    iot_callback callback = {send_data_to_sock,iot_on_connect,iot_on_message,&user_data};
+    iot_callback callback = {send_data_to_sock,on_iot_connect,on_iot_message,&user_data};
 
     //创建iot对象
     user_data._ctx = iot_context_alloc(&callback);
     //开始登陆iot服务器
-    iot_send_connect_pkt(user_data._ctx,CLIENT_ID,SECRET,USER_NAME);
-
-    //设置socket接收超时时间，两秒超时一次，目的是产生一个定时器
-    net_set_sock_timeout(user_data._fd ,1,2);
+    iot_send_connect_pkt(user_data._ctx,s_client_id,s_secret,s_user_name);
 
     //socket接收buffer
     char buffer[1024];
@@ -214,11 +209,6 @@ void run_main(){
             //服务器断开连接
             LOGE("read eof\r\n");
             break;
-        }
-        if(recv == -1 ){
-            //接收超时，触发定时器
-            on_timer_tick(&user_data);
-            continue;
         }
         //收到数据，输入到iot对象
         iot_input_data(user_data._ctx,buffer,recv);
